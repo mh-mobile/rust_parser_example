aac6c0ed6fb22fe6385088c16000fc434f67c8c4
mh-mobile <mh.mobiler@gmail.com>
2022-01-02 17:00:08 +0900

    インタープリタの実装

--- src/main.rs
+++ src/main.rs
@@ -90,6 +90,32 @@ impl fmt::Display for Error {
     }
 }
 
+impl fmt::Display for InterpreterError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        use self::InterpreterErrorKind::*;
+        match self.value {
+            DivisionByZero => write!(f, "division by zero"),
+        }
+    }
+}
+
+impl StdError for InterpreterError {
+    fn description(&self) -> &str {
+    use self::InterpreterErrorKind::*;
+    match self.value {
+        DivisionByZero => "the right hand expression of the division evaluate to zero",
+    } 
+        
+    }
+}
+
+impl InterpreterError {
+    fn show_diagnostic(&self, input: &str) {
+        eprintln!("{}", self);
+        print_annot(input, self.loc.clone());
+    }
+}
+
 impl StdError for LexError {}
 
 impl StdError for ParseError {}
@@ -142,6 +168,66 @@ fn show_trace<E: StdError>(e: E) {
     }
 }
 
+struct Interpreter;
+
+impl Interpreter {
+    pub fn new() -> Self {
+        Interpreter
+    }
+
+    pub fn eval(&mut self, expr: &Ast) -> Result<i64, InterpreterError> {
+        use self::AstKind::*;
+
+        match expr.value {
+            Num(n) => Ok(n as i64),
+            UniOp { ref op, ref e} => {
+                let e = self.eval(e)?;
+                Ok(self.eval_uniop(op, e))
+            }
+            BinOp {
+                ref op, 
+                ref l,
+                ref r,
+            } => {
+                let l = self.eval(l)?;
+                let r = self.eval(r)?;
+                self.eval_binop(op, l, r).map_err(|e| InterpreterError::new(e, expr.loc.clone()))
+            }
+        }
+    }
+
+    fn eval_uniop(&mut self, op: &UniOp, n: i64) -> i64 {
+        use self::UniOpKind::*;
+        match op.value {
+            Plus => n,
+            Minus => -n,
+        }
+    }
+
+    fn eval_binop(&mut self, op: &BinOp, l: i64, r: i64) -> Result<i64, InterpreterErrorKind> {
+        use self::BinOpKind::*;
+        match op.value {
+            Add => Ok(l + r),
+            Sub => Ok(l - r),
+            Mult => Ok(l * r),
+            Div => {
+                if r == 0 {
+                    Err(InterpreterErrorKind::DivisionByZero)
+                } else {
+                    Ok(l / r)
+                }
+            }
+        }
+    }
+
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum InterpreterErrorKind {
+    DivisionByZero,
+}
+
+type InterpreterError = Annot<InterpreterErrorKind>;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 struct Loc(usize, usize);
@@ -563,6 +649,8 @@ fn main() {
     let stdin = BufReader::new(stdin);
     let mut lines = stdin.lines();
 
+    let mut interp = Interpreter::new();
+
     loop {
         prompt("> ").unwrap();
         if let Some(Ok(line)) = lines.next() {
@@ -574,7 +662,15 @@ fn main() {
                     continue;
                 }
             };
-            println!("{:?}", ast);
+            let n = match interp.eval(&ast) {
+                Ok(n) => n,
+                Err(e) => {
+                    e.show_diagnostic(&line);
+                    show_trace(e);
+                    continue;
+                }
+            };
+            println!("{}", n);
         } else {
             break;
         }
